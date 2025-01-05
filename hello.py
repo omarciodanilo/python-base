46e072861aa6a8b6e8e4681fd8d8c2d665782702
Márcio Almeida <16402947+omarciodanilo@users.noreply.github.com>
2025-01-05 17:52:46 -0300

    Implementado o Hello World Multi Idiomas

--- hello.py
+++ hello.py
@@ -1,11 +1,37 @@
 #!/usr/bin/env python3
 
-# Imprime mensagem na tela
+"""Hello World Multi Idiomas.
 
-"""
-Comentário
-multi
-linhas
+Descrição:
+    Dependendo do idioma configurado no ambiente (variável LANG),
+    o programa exibirá a mensagem correspondente.
+
+Utilização:
+    Tenha a variável LANG devidamente configurada.
+    Exemplo: export LANG=pt_BR
+
+Execução:
+    python3 hello.py
+    ou
+    ./hello.py
 """
 
-print("Hello, World!")
+__version__ = "0.0.1"
+__author__ = "Márcio Almeida"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG", "en_US")[:5]
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+elif current_language == "es_SP":
+    msg = "Hola, Mundo!"
+elif current_language == "fr_FR":
+    msg = "Bonjour, Monde!"
+
+print(msg)
